dcebcd124f91d9d4087b2bc7b4dd46b7bb8a125a
Rolando Domínguez <rolando.fcfm2020@gmail.com>
2020-09-24 21:00:53 -0500

    Removi lineas incesarias de paquetes.

--- ApiTokenJWT/Controllers/LoginController.cs
+++ ApiTokenJWT/Controllers/LoginController.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 using ApiTokenJWT.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
