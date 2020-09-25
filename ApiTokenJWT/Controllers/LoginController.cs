4386174d0ef2b06e03eecfb35ed970219f5595b7
Rolando Domínguez <rolando.fcfm2020@gmail.com>
2020-09-24 21:18:44 -0500

    Cambio nomrbe usuario

--- ApiTokenJWT/Controllers/LoginController.cs
+++ ApiTokenJWT/Controllers/LoginController.cs
@@ -26,10 +26,10 @@ namespace ApiTokenJWT.Controllers
 
         [AllowAnonymous]
         [HttpPost]
-        public IActionResult Login([FromBody] UserModel userLogin)
+        public IActionResult Login([FromBody] UserModel myUserLogin)
         {
             IActionResult response = Unauthorized();
-            var user = AuthenticateUser(userLogin);
+            var user = AuthenticateUser(myUserLogin);
 
             if (user != null)
             {
@@ -47,7 +47,7 @@ namespace ApiTokenJWT.Controllers
             var permClaims = new List<Claim>();
             permClaims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
             permClaims.Add(new Claim("valid", "1"));
-            permClaims.Add(new Claim("userid", "1"));
+            permClaims.Add(new Claim("userid", userInfo.Username));
             permClaims.Add(new Claim("name", userInfo.EmailAddress));
 
 
